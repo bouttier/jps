127a372e9d6982d9787449e97e8f0e3ec3b08ff9
Élie Bouttier <elie.bouttier@free.fr>
2012-12-26 16:20:19 +0100

    Add references, README and copyrights

--- libjps.h
+++ libjps.h
@@ -4,8 +4,10 @@
  * JPS (Jump Point Search). L’algorithme JPS permet d’éviter de développer
  * certains nœuds inutilement, tous en garantissant que le chemin obtenu est de
  * coup minimal.
- * Publication originale :
+ * Sources :
  *      http://harablog.wordpress.com/2011/09/07/jump-point-search/
+ *      http://grastien.net/ban/articles/hg-aaai11.pdf
+ *      http://github.com/arirahikkala/astar-jps
  */
 #ifndef _LIBJPS_H_
 #define _LIBJPS_H_
